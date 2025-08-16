c2bb247bdea31bb283c8c84abedf20c1abd32649
Daniel Schwarz <dan@butter.sh>
2025-08-17 00:54:32 +0200

    fix type check error

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+// this was needed because the onBeforeinput event was expected by the typescript compiler
+import '@vue/runtime-dom';
+
+declare module '@vue/runtime-dom' {
+  interface HTMLAttributes {
+    onBeforeinput?: (event: InputEvent) => void;
+  }
+}
