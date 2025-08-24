03ded4423b11e4d4169a506d8b988c8e63c5177f
Daniel Schwarz <dan@butter.sh>
2025-08-25 00:40:12 +0200

    modify date + numeric playgrounds to show multiple values

--- src/components/playground-sections/DatePlayground.vue
+++ src/components/playground-sections/DatePlayground.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="playground-masks">
     <label>Date Mask ISO</label>
-    <MaskedText :mask="dateMaskISO" v-model="dateValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="dateMaskISO" v-model="dateValue1" @update:debug-info="updateDebugInfo" />
 
     <label>Date Mask DE</label>
-    <MaskedText :mask="dateMaskDE" v-model="dateValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="dateMaskDE" v-model="dateValue1" @update:debug-info="updateDebugInfo" />
 
     <label>Date Mask EN</label>
-    <MaskedText :mask="dateMaskEN" v-model="dateValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="dateMaskEN" v-model="dateValue2" @update:debug-info="updateDebugInfo" />
 
     <label>Date Mask US</label>
-    <MaskedText :mask="dateMaskUS" v-model="dateValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="dateMaskUS" v-model="dateValue2" @update:debug-info="updateDebugInfo" />
 
     <label>Date Mask JP</label>
-    <MaskedText :mask="dateMaskJP" v-model="dateValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="dateMaskJP" v-model="dateValue3" @update:debug-info="updateDebugInfo" />
 
     <label>Date Mask KR</label>
-    <MaskedText :mask="dateMaskKR" v-model="dateValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="dateMaskKR" v-model="dateValue3" @update:debug-info="updateDebugInfo" />
   </div>
 </template>
 
@@ -25,7 +25,9 @@
   import { type MaskDerivedState, MaskedText, type MaskState } from '@/lib/masked-text';
   import { DateMask } from '@/lib/masked-text/masks';
 
-  const dateValue = ref({ year: '', month: '', day: '' } as Record<string, string>);
+  const dateValue1 = ref({ year: '2025', month: '09', day: '01' } as Record<string, string>);
+  const dateValue2 = ref({ year: '2000', month: '2', day: '29' } as Record<string, string>);
+  const dateValue3 = ref({ year: '', month: '', day: '' } as Record<string, string>);
 
   const dateMaskISO = DateMask('iso');
   const dateMaskDE = DateMask('de');
--- src/components/playground-sections/NumericPlayground.vue
+++ src/components/playground-sections/NumericPlayground.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="playground-masks">
     <label>Date Mask Basic</label>
-    <MaskedText :mask="numericMaskBasic" v-model="numericValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="numericMaskBasic" v-model="numericValue1" @update:debug-info="updateDebugInfo" />
 
     <label>Date Mask Comma Separator</label>
-    <MaskedText :mask="numericMaskCommaSeparator" v-model="numericValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="numericMaskCommaSeparator" v-model="numericValue1" @update:debug-info="updateDebugInfo" />
 
     <label>Date Mask Space Separator</label>
-    <MaskedText :mask="numericMaskSpaceSeparator" v-model="numericValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="numericMaskSpaceSeparator" v-model="numericValue2" @update:debug-info="updateDebugInfo" />
 
     <label>Date Mask European</label>
-    <MaskedText :mask="numericMaskEuropean" v-model="numericValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="numericMaskEuropean" v-model="numericValue2" @update:debug-info="updateDebugInfo" />
 
     <label>Date Mask with Suffix</label>
-    <MaskedText :mask="numericMaskWithSuffix" v-model="numericValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="numericMaskWithSuffix" v-model="numericValue3" @update:debug-info="updateDebugInfo" />
 
     <label>Date Mask with Prefixes</label>
-    <MaskedText :mask="numericMaskWithPrefixes" v-model="numericValue" @update:debug-info="updateDebugInfo" />
+    <MaskedText :mask="numericMaskWithPrefixes" v-model="numericValue3" @update:debug-info="updateDebugInfo" />
   </div>
 </template>
 
@@ -25,7 +25,9 @@
   import { type MaskDerivedState, MaskedText, MaskSectionFixed, type MaskState } from '@/lib/masked-text';
   import { NumericMask } from '@/lib/masked-text/masks';
 
-  const numericValue = ref({ integers: '', decimals: '' } as Record<string, string>);
+  const numericValue1 = ref({ integers: '1234567890', decimals: '0987654321' } as Record<string, string>);
+  const numericValue2 = ref({ integers: '', decimals: '' } as Record<string, string>);
+  const numericValue3 = ref({ integers: '12', decimals: '99' } as Record<string, string>);
 
   const numericMaskBasic = NumericMask({
     decimalSeparator: '.',
