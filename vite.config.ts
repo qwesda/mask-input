2299fa58a11dbab868d899546ad806f6f2d2a89a
Daniel Schwarz <dan@butter.sh>
2025-08-25 01:50:29 +0200

    update `vite.config.ts` for webstorm debugger suppoer

--- vite.config.ts
+++ vite.config.ts
@@ -24,4 +24,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    open: false,
+  },
+  dev: {
+    sourcemap: true,
+  },
 });
